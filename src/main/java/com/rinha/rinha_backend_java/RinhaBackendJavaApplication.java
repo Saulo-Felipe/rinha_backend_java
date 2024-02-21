24d9728fcaeca2fcfca2fc30df921cc59cd648aa
Saulo-Felipe <saulofelipe234567@gmail.com>
2024-02-20 21:53:52 -0300

    init

---
+++ src/main/java/com/rinha/rinha_backend_java/RinhaBackendJavaApplication.java
@@ -0,0 +1,13 @@
+package com.rinha.rinha_backend_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RinhaBackendJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RinhaBackendJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rinha/rinha_backend_java/RinhaBackendJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rinha.rinha_backend_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RinhaBackendJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
